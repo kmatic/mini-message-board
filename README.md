2eb75d0129b43831318f986d686aba7fc2f4ff0d
Kristopher <kristopher_matic@hotmail.com>
2022-10-20 15:46:14 -0700

    finish simple mini messageboard

---
+++ README.md
@@ -0,0 +1,2 @@
+# mini-message-board
+Mini message board app built with Node.js &amp; Express
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'HOLA!',
+    user: 'Kris',
+    added: new Date()
+  },
+  {
+    text: 'Greetings, good sir!',
+    user: 'Sammi',
+    added: new Date()
+  },
+  {
+    text: 'dasiojndiasdnasjkdna',
+    user: 'YERR',
+    added: new Date()
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini Messageboard'});
+});
+
+router.post('/new', function(req, res, next) {
+    messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+    res.redirect('/');
 });
 
 module.exports = router;
